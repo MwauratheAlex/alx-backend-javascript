b94698f2264c93cf4ea1bef6f4cda82eb8b67f33
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-26 08:36:02 +0300

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const resultArray = [...array];
-
-  for (const [idx, val] of resultArray.entries()) {
-    resultArray[idx] = appendString + val;
+  for (const [idx, val] of array.entries()) {
+    array[idx] = appendString + val;
   }
 
-  return resultArray;
+  return array;
 }
