81b72cc427506ccc23429a384c708bb93fc0be2b
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-09-27 12:45:00 +0300

    7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload]).then(res => res);
+  return Promise.any([chinaDownload, USDownload]);
 }
