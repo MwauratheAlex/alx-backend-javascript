f8f53f4ddcf09523887849bcee9674574a188c96
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-03 13:19:53 +0300

    8-clean_set.js: validate args

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
 
   const results = [];
 
