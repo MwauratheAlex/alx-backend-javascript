8352e9ac493d3dfded9188b16b015e320f0133a4
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-03 09:46:44 +0300

    2-get_students_by_loc.js

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+export default function getStudentsByLocation(students, city) {
+  if (Array.isArray(students) === false) return [];
+
+  return students.filter((student) => student.location === city);
+}
