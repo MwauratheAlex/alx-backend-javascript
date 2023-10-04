be47c19b88c87acbd8a9a81bf90420e983c14ef4
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-04 15:44:20 +0300

    task 2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -7,7 +7,7 @@ interface DirectorInterface {
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak?(): string;
-  workTeacherTasks?(): string;
+  workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
@@ -29,11 +29,11 @@ class Teacher implements TeacherInterface {
     return "Cannot work from home";
   }
 
-  getToWork() {
+  getCoffeeBreak() {
     return "Cannot have a break";
   }
 
-  workDirectorTasks() {
+  workTeacherTasks() {
     return "Getting to work";
   }
 }
@@ -45,6 +45,17 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee("$500"));
+function isDirector(employee: Teacher | Director): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
