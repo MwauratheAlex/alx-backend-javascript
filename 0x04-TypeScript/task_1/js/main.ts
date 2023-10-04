264ff990aeb04018e2ac09ac68c725b2565a6304
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-04 10:24:10 +0300

    task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,47 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface PrintTeacher {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacher = (firstName: string, lastName: string) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
