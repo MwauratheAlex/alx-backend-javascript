41461b439399d4e66689cf7d40e3f8a8de745913
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-21 11:37:46 +0300

    add: 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+/**
+ * This script prompts the user to enter their name and displays it.
+ * It listens for user input from the standard input stream (stdin)
+ *  and writes the name to the standard output stream (stdout).
+ * The script terminates when the user ends the input stream.
+ */
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
