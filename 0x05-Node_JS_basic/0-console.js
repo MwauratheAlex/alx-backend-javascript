f0fad9d574142a918b098c4fa53bcea92a317aaa
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-21 10:39:03 +0300

    add: 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * Displays a message to the console.
  * @param {string} message - The message to be displayed.
  */
-function displayMessage (message) {
+const displayMessage = (message) => {
   console.log(message);
-}
+};
 
 module.exports = displayMessage;
