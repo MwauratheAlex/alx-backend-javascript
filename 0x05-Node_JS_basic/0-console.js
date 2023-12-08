bfb4ed31746692e154efeb2adb746b3cc0e93f95
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-12-08 20:58:42 +0300

    remove: 0x05

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,9 +0,0 @@
-/**
- * Displays a message to the console.
- * @param {string} message - The message to be displayed.
- */
-const displayMessage = (message) => {
-  console.log(message);
-};
-
-module.exports = displayMessage;
