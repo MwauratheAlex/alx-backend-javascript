bfb0df8b4b861bf90fd233b3ebfddff147c07070
Mbugua <mwaurathealex@gmail.com>
2024-02-01 12:02:00 +0300

    reading file sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,30 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
+  if (!(fs.existsSync(path) && fs.statSync(path).isFile())) {
+    throw new Error('Cannot load the database');
+  }
+  const students = fs
+    .readFileSync(path, 'utf8')
+    .trim()
+    .split('\n');
+  console.log(`Number of students: ${students.length - 1}`);
+
+  const fieldStudents = {};
+  for (let i = 1; i < students.length; i += 1) {
+    const studentData = students[i].trim().split(',');
+    const firstName = studentData[0];
+    const field = studentData[studentData.length - 1];
+    if (!(field in fieldStudents)) {
+      fieldStudents[field] = [];
     }
-    const students = data.trim().split('\n');
-    const noOfStudents = students.length - 1;
-    const fieldStudents = {};
-    for (let i = 1; i < students.length; i += 1) {
-      const studentData = students[i].trim().split(',');
-      const firstName = studentData[0];
-      const field = studentData[studentData.length - 1];
-      if (!(field in fieldStudents)) {
-        fieldStudents[field] = [];
-      }
-      fieldStudents[field].push(firstName);
-    }
-    console.log(`Number of students: ${noOfStudents}`);
-    for (const field of Object.keys(fieldStudents)) {
-      const studentsInField = fieldStudents[field];
-      const noStudents = studentsInField.length;
-      const studentsStr = studentsInField.join(', ');
-      console.log(`Number of students in ${field}: ${noStudents}. List: ${studentsStr}`);
-    }
-  });
+    fieldStudents[field].push(firstName);
+  }
+  for (const field of Object.keys(fieldStudents)) {
+    const studentsInField = fieldStudents[field];
+    const listOfFirstNames = studentsInField.join(', ');
+    console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${listOfFirstNames}`);
+  }
 }
 
 module.exports = countStudents;
