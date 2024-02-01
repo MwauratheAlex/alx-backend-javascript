6305fd95cade906ba608d78989a0f5a20530b87a
Mbugua <mwaurathealex@gmail.com>
2024-02-01 13:05:52 +0300

    read file async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const students = data.trim().split('\n');
+        console.log(`Number of students: ${students.length - 1}`);
+        const fieldStudents = {};
+        for (let i = 1; i < students.length; i += 1) {
+          const studentData = students[i].trim().split(',');
+          const firstName = studentData[0];
+          const field = studentData[studentData.length - 1];
+          if (!(field in fieldStudents)) {
+            fieldStudents[field] = [];
+          }
+          fieldStudents[field].push(firstName);
+        }
+        for (const field of Object.keys(fieldStudents)) {
+          const studentsInField = fieldStudents[field];
+          const noStudentsInFeild = studentsInField.length;
+          const studentList = studentsInField.join(', ');
+          console.log(`Number of students in ${field}: ${noStudentsInFeild}. List: ${studentList}`);
+        }
+        resolve(true);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
