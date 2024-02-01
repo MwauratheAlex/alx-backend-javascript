b3f8f597da03d41f369717024ec2cb1b59e342e2
Mbugua <mwaurathealex@gmail.com>
2024-02-01 15:18:27 +0300

    complex http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,29 +1,31 @@
 const http = require('http');
+
 const port = 1245;
 const hostname = 'localhost';
 const databaseFile = (process.argv.length) > 2 ? process.argv[2] : '';
 const countStudents = require('./3-read_file_async');
 
 const requestHandler = (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    const url = req.url;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const { url } = req;
 
-    if (url === '/') {
-        res.end('Hello Holberton School!');
-        return;
-    } else if (url === '/students') {
-        const resData = 'This is the list of our students\n';
-        countStudents(databaseFile)
-          .then((report) => {
-            console.log(report);
-            res.end(resData);
-            return;
-          })
-        return;
-    }
-    res.end();
-}
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+    return;
+  } if (url === '/students') {
+    const title = 'This is the list of our students\n';
+    countStudents(databaseFile)
+      .then((report) => {
+        res.end(`${title}${report}`);
+      })
+      .catch((err) => {
+        res.end(err.toString());
+      });
+    return;
+  }
+  res.end();
+};
 
 const app = http.createServer(requestHandler);
 
