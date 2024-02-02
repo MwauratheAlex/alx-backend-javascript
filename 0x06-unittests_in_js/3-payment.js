ecd435ab3534bed784c1a44d1b5a87bdfd14099e
Mbugua <mwaurathealex@gmail.com>
2024-02-02 12:03:39 +0300

    testing

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmout, totalShipping) {
   const total = Utils.calculateNumber('SUM', totalAmout, totalShipping);
-  console.log(`The total is ${total}`);
+  console.log(`The total is: ${total}`);
 }
 
 module.exports = sendPaymentRequestToApi;
