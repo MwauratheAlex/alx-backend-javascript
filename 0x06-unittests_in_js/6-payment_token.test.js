102daabee91f27af59181b9d0cc4729f96c1e013
Mbugua <mwaurathealex@gmail.com>
2024-02-02 16:09:01 +0300

    test async funtion

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,15 +1,14 @@
 const { expect } = require('chai');
-const sinon = require('sinon');
 const getPaymentTokenFromAPI = require('./6-payment_token.js');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('returns the correct value', () => {
+  it('returns the correct value', (done) => {
     expect(getPaymentTokenFromAPI(false)).undefined;
     getPaymentTokenFromAPI(true)
       .then((data) => {
-        expect(data).to.be({
-            data: 'Successful response from the API'
-        })
+        expect(data).deep.equal({
+          data: 'Successful response from the API'
+        });
         done();
       });
   });
