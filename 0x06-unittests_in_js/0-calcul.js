64e8fe82542f73cc288f50915a7bcafea2fa8c1f
Mbugua <mwaurathealex@gmail.com>
2024-02-02 08:51:27 +0300

    mocha install

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+function calculateNumber(a, b) {
+  return Math.ceil(a + b);
+}
+
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+const calculateNumber = require('./0-calcul.js');
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+  it('whole numbers', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('decimal and whole number (rounding)', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('two decimal (rounding)', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('two decimal (ceil)', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+})
+
