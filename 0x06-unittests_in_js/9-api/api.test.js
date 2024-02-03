74ea3ffd419c120fad9dc61c5756bb61f28daa5c
Mbugua <mwaurathealex@gmail.com>
2024-02-03 13:13:58 +0300

    testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,14 +3,14 @@ const request = require('request');
 
 describe('Payment system api', () => {
   const url = 'http://localhost:7865';
-  it('GET / returns correct value and code', (done) => {
+  it('GET / returns "Welcome to the payment system"', (done) => {
     request.get(url, (err, res, body) => {
       expect(res.statusCode).equal(200);
       expect(body).equal('Welcome to the payment system');
       done();
     });
   });
-  it('GET /cart/:id returns correct value and code', (done) => {
+  it('GET /cart/:id returns "Payment methods for cart :id"', (done) => {
     const id = 777;
     request.get(`${url}/cart/${id}`, (err, res, body) => {
        expect(res.statusCode).equal(200);
