ecf89ddc514ea224f3edb56c0940c3777bc063b9
Mbugua <mwaurathealex@gmail.com>
2024-02-03 13:55:42 +0300

    done

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -12,21 +12,21 @@ describe('Payment system api', () => {
   });
   it('GET /cart/:id returns "Payment methods for cart :id"', (done) => {
     request.get(`${url}/cart/777`, (err, res, body) => {
-       expect(res.statusCode).to.be.equal(200);
-       expect(res.body).to.be.equal('Payment methods for cart 777');
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.body).to.be.equal('Payment methods for cart 777');
+      done();
     });
-    done();
   });
   it('GET /cart/:id returns 404 error when id is negative', (done) => {
     request.get(`${url}/cart/-777`, (err, res, body) => {
-       expect(res.statusCode).to.be.equal(404);
+      expect(res.statusCode).to.be.equal(404);
+      done();
     });
-    done();
   });
   it('GET /cart/:id returns returns 404 error when id is not number', (done) => {
     request.get(`${url}/cart/mbugua`, (err, res, body) => {
-       expect(res.statusCode).to.be.equal(404);
+      expect(res.statusCode).to.be.equal(404);
+      done();
     });
-    done();
   });
 });
