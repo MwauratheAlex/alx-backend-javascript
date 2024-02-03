0739604ab0f4a28aced808c43221ebc12e362d18
Mbugua <mwaurathealex@gmail.com>
2024-02-03 13:50:03 +0300

    to be or not to be

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -5,14 +5,14 @@ describe('Payment system api', () => {
   const url = 'http://localhost:7865';
   it('GET / returns "Welcome to the payment system"', (done) => {
     request.get(url, (err, res, body) => {
-      expect(res.statusCode).equal(200);
+      expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
   it('GET /cart/:id returns "Payment methods for cart :id"', (done) => {
     request.get(`${url}/cart/777`, (err, res, body) => {
-       expect(res.statusCode).equal(200);
+       expect(res.statusCode).to.be.equal(200);
        expect(res.body).to.be.equal('Payment methods for cart 777');
     });
     done();
