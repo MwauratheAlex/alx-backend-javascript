5735f714b2ff18f33dc0a879f3f6012d6f7a31c6
Mbugua <mwaurathealex@gmail.com>
2024-06-06 08:31:43 +0300

    lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write('Your name is: ' + input);
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write('Your name is: ' + name);
   }
 });
 
