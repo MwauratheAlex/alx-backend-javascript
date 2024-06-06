539cc5c6c78ed3a62557a702950aeb366a824bf6
Mbugua <mwaurathealex@gmail.com>
2024-06-06 09:46:46 +0300

    2 lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 
-
 function countStudents(path) {
   if (!(fs.existsSync(path) && fs.statSync(path).isFile())) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
   const students = fs
     .readFileSync(path, { encoding: 'utf8' })
     .trim().split('\n');
 
-
   const studentField = {};
   console.log(`Number of students: ${students.length - 1}`);
 
-  for (let i = 1; i < students.length; i++) {
+  for (let i = 1; i < students.length; i += 1) {
     const studentData = students[i].trim().split(',');
     const field = studentData[studentData.length - 1];
     const firstname = studentData[0];
@@ -24,12 +22,13 @@ function countStudents(path) {
     studentField[field].push(firstname);
   }
 
-  Object.keys(studentField).forEach(field => {
+  Object.keys(studentField).forEach((field) => {
     const fnames = studentField[field];
     const l = fnames.join(', ');
 
     console.log(
-      `Number of students in ${field}: ${fnames.length}. List: ${l}`);
+      `Number of students in ${field}: ${fnames.length}. List: ${l}`,
+    );
   });
 }
 
