6df8690e3fdb8b3080b69a24342202adf2ae5c38
Mbugua <mwaurathealex@gmail.com>
2024-06-07 11:05:59 +0300

    stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,19 +1,14 @@
-const { expect } = require('chai');
-const { it, describe } = require('mocha');
 const sinon = require('sinon');
-
 const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./4-payment.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+const { expect } = require('chai');
 
-describe('', () => {
-  const checkSoy = sinon.spy(console, 'log');
-  it('checking if numbers round with spies and stubs', () => {
+describe('sendPaymentRequestToApi', () => {
+  it('stub for utils.calculateNumber once with correct values', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.returns(10);
     sendPaymentRequestToApi(100, 20);
-    const stubBoy = sinon.stub(Utils, 'calculateNumber');
-    stubBoy.withArgs('SUM', 100, 20).returns(120);
-    expect(checkSoy.calledOnce).to.be.true;
-    expect(console.log('The total is: 120')).to.be.all;
-    checkSoy.restore();
-    stubBoy.restore();
+    expect(stub.calledOnceWith('SUM', 100, 20)).to.be.true;
+    stub.restore();
   });
 });
